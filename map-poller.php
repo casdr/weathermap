a1ebcc4fd45f2ce789b46461eb0d87421b780cd4
laf <neil@lathwood.co.uk>
2013-12-10 16:28:48 +0000

    Added GPL license Removed dependency on $pathd variable Updated chdir code to use path called from
     - Big thanks to Supun Rathnayake (https://twitter.com/supunr)

--- map-poller.php
+++ map-poller.php
@@ -1,11 +1,33 @@
 #!/usr/bin/env php
 <?php
 
+// Copyright (C) 2013 Neil Lathwood neil@lathwood.co.uk
+/**
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
 // Set variables for map-poller.php
 $weathermap_url = '/weathermap/';
-//$pathd='/opt/librenms/html/weathermap';
-$pathd='';// THIS NEEDS TO BE SET
-chdir($pathd);
+
+// Change to directory that map-poller was run from.
+// Thank you to Supun Rathnayake (https://twitter.com/supunr) for the bug report
+// and fix for includes being set incorrectly and changing map-poller to chdir from
+// where it's run.
+
+chdir(dirname($argv[0]));
 
 if(empty($pathd)) {
 	echo 'Please set $pathd';
